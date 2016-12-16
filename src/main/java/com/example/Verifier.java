54cf432baf9dc37ba3b5666c1666a2aca7e58000
Taichi Uragami <backpaper0@gmail.com>
2016-12-17 08:17:46 +0900

    Hash with local date

--- src/main/java/com/example/Verifier.java
+++ src/main/java/com/example/Verifier.java
@@ -2,9 +2,11 @@ package com.example;
 
 import java.security.GeneralSecurityException;
 import java.security.Key;
+import java.time.LocalDate;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -19,7 +21,9 @@ public class Verifier {
     public String hash(Long value) throws GeneralSecurityException {
         Mac mac = Mac.getInstance("HmacSHA256");
         mac.init(key);
-        byte[] hash = mac.doFinal(String.valueOf(value).getBytes());
+        byte[] source = Stream.of(value, LocalDate.now()).map(Objects::toString)
+                .collect(Collectors.joining(":")).getBytes();
+        byte[] hash = mac.doFinal(source);
         return IntStream.range(0, hash.length).mapToObj(i -> String.format("%02x", hash[i]))
                 .collect(Collectors.joining());
     }
